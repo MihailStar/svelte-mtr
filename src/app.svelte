c7ade256d0e55d17b057ad7b91403e17498984ac
MihailStar <mhlstr@mail.ru>
2020-11-16 13:20:28 +0300

    feat: добавляет пример

---
+++ src/app.svelte
@@ -0,0 +1,25 @@
+<script>
+  const name = 'Rich Harris';
+  const src = 'https://avatars0.githubusercontent.com/u/1162160';
+  const html = `<div>&copy; ${name}</div>`;
+</script>
+
+<style>
+  :global(.wrapper) {
+    margin: 0 auto;
+    max-width: 960px;
+    min-width: 320px;
+  }
+
+  main {
+    text-align: center;
+  }
+</style>
+
+<main>
+  <div class="wrapper">
+    <h1>{name}</h1>
+    <img {src} alt="{name}" />
+    {@html html}
+  </div>
+</main>
