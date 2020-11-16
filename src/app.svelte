e424e5a093f2ce36e63fc4b394833c6377f0fb00
MihailStar <mhlstr@mail.ru>
2020-11-16 13:25:27 +0300

    feat: добавляет пример

---
+++ src/app.svelte
@@ -0,0 +1,49 @@
+<script>
+  let title = 'Svelte';
+  let pos = {
+    x: 0,
+    y: 0,
+  };
+  let inputValue = '';
+
+  function clickHandler() {
+    title += '!';
+  }
+
+  function mousemoveHandler(e) {
+    [pos.x, pos.y] = [e.x, e.y];
+  }
+</script>
+
+<style>
+  :global(body) {
+    text-align: center;
+  }
+
+  div {
+    background-color: #4169e1;
+    height: 320px;
+    margin: 0 auto;
+    margin-bottom: 1em;
+    width: 320px;
+  }
+</style>
+
+<h1>title: {title}</h1>
+<button type="button" on:click="{clickHandler}">Change title</button>
+
+<div on:mousemove="{mousemoveHandler}">
+  <h2>x: {pos.x}, y: {pos.y}</h2>
+</div>
+
+<form on:submit|preventDefault>
+  <input
+    type="text"
+    on:input="{(e) => {
+      e.preventDefault();
+      inputValue = e.target.value;
+    }}"
+  />
+  <input type="text" value="{`inputValue: ${inputValue}`}" disabled />
+  <button>Submit form</button>
+</form>
