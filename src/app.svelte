3e15e8d15308c6e649ebb9608e2d57776278b87a
MihailStar <mhlstr@mail.ru>
2020-11-16 13:50:30 +0300

    feat: добавляет пример

---
+++ src/app.svelte
@@ -0,0 +1,42 @@
+<script>
+  import Person from './person.svelte';
+
+  let persons = [
+    { id: 1, name: 'Максим', age: 20, job: 'Frontend' },
+    { id: 2, name: 'Елена', age: 20, job: 'Design' },
+    { id: 3, name: 'Игорь', age: 20, job: 'Backend' },
+  ];
+
+  let value = '';
+
+  function addPerson() {
+    persons = persons.concat({
+      id: Date.now(),
+      name: value,
+    });
+
+    // persons = [
+    //   ...persons,
+    //   {
+    //     id: Date.now(),
+    //     name: value,
+    //   },
+    // ];
+
+    value = '';
+  }
+
+  function removeFirstPerson() {
+    persons = persons.slice(1);
+  }
+</script>
+
+<input type="text" bind:value />
+<button on:click="{addPerson}">Add person</button>
+<button on:click="{removeFirstPerson}">Remove first person</button>
+
+{#each persons as { name, id }, index (id)}
+  <Person {name} {index} />
+{:else}
+  <p>No persons</p>
+{/each}
---
+++ src/person.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let name = 'Undefined name';
+  export let index;
+
+  const copy = name;
+</script>
+
+<p>
+  <b>{index + 1}</b>
+  {name} / {copy}
+</p>
