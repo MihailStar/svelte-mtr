406c2262a88cfeaf6ec99777030d766bf98abfe9
MihailStar <mhlstr@mail.ru>
2020-11-16 13:45:35 +0300

    feat: добавляет пример

---
+++ src/app.svelte
@@ -0,0 +1,24 @@
+<script>
+  import People from './people.svelte';
+
+  const peoples = [
+    { name: 'Максим', age: 20, job: 'Frontend' },
+    { name: 'Елена', age: 20, job: 'Design' },
+  ];
+
+  const name = 'Игорь';
+  const age = 20;
+  const jobTitle = 'Backend';
+</script>
+
+<People />
+
+<People
+  name="{peoples[0].name}"
+  age="{peoples[0].age}"
+  job="{peoples[0].job}"
+/>
+
+<People {...peoples[1]} />
+
+<People {name} {age} job="{jobTitle}" />
---
+++ src/people.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let name = 'Undefined name';
+  export let age = 'Undefined age';
+  export let job = 'Undefined job';
+</script>
+
+<style>
+  table {
+    border: 1px solid currentColor;
+  }
+</style>
+
+<table>
+  <tr>
+    <th>Name</th>
+    <th>Age</th>
+    <th>Job</th>
+  </tr>
+  <tr>
+    <td>{name}</td>
+    <td>{age}</td>
+    <td>{job}</td>
+  </tr>
+</table>
